6438ec3c4d8478379cbbf6ba3e3449692dd07e4e
araborg <arababs2015@gmail.com>
2026-05-27 17:44:52 +0100

    profile_card: 4

--- 1/profile/src/App.tsx
+++ 1/profile/src/App.tsx
@@ -1,6 +1,40 @@
 import "./App.css";
 
+interface Data {
+	skill: string;
+	emoji: string;
+	color: string;
+	level: string;
+}
+
+const data: Data = [
+	{ skill: "HRML+CSS", emoji: "✅", color: "blue", level: "beginner" },
+
+	{
+		skill: "JavaScript",
+		emoji: "✅",
+		color: "yellow",
+		level: "intermediate",
+	},
+
+	{
+		skill: "Web Design",
+		emoji: "✅",
+		color: "lightgreen",
+		level: "beginner",
+	},
+
+	{ skill: "Git and GitHub", emoji: "✅", color: "red", level: "beginner" },
+
+	{ skill: "React", emoji: "✅", color: "rebeccapurple", level: "advanced" },
+
+	{ skill: "Svelte", emoji: "✅", color: "orangered", level: "intermediate" },
+];
+
 function App() {
+	const dataArr = data;
+	console.log(dataArr);
+
 	return (
 		<>
 			<p>Welcome</p>

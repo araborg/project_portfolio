be4ca6edec3f90a6b731649bd4d756ea6f170c02
araborg <arababs2015@gmail.com>
2026-05-28 09:28:12 +0100

    profile_card: 16

--- 1/profile/src/App.tsx
+++ 1/profile/src/App.tsx
@@ -1,15 +1,20 @@
 import "./App.css";
 
 import Avatar from "./Avatar";
+import Intro from "./Intro";
 import SkillList from "./SkillList";
 
 function App() {
 	return (
-		<>
+		<div className="card">
 			<Avatar />
 
-			<SkillList />
-		</>
+			<main>
+				<Intro />
+
+				<SkillList />
+			</main>
+		</div>
 	);
 }
 

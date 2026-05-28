9d0adc02eec0e08bec398d54e652bbd55c3ea3cb
araborg <arababs2015@gmail.com>
2026-05-28 19:54:43 +0100

    profile_card: 23

--- 1/profile/src/Avatar.tsx
+++ 1/profile/src/Avatar.tsx
@@ -1,7 +1,17 @@
+interface ImageProps {
+	src: string;
+	alt: string;
+	className: string;
+}
+
+function Image({ src }: ImageProps) {
+	return <img src={src} />;
+}
+
 function Avatar() {
 	const avatarImg = "/salamino.jpg";
 
-	return <img src={avatarImg} alt="Avatar" className="avatar" />;
+	return <Image src={avatarImg} alt="avatar" className="avatar" />;
 }
 
 export default Avatar;

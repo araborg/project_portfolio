6f4750d0466950441907f8962310bfbb7a546693
araborg <arababs2015@gmail.com>
2026-06-12 22:48:43 +0100

    flashcard: 32

--- fundamtal/flash_card/src/App.tsx
+++ fundamtal/flash_card/src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import Flashcard from "./Flashcard";
 
 function App() {
-	return <Flashcard />;
+	return (
+		<div className="container">
+			<Flashcard />
+		</div>
+	);
 }
 
 export default App;

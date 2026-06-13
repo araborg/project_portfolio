f2eb3e81674bca18e3d7c6b55e68a5b77c513c24
araborg <arababs2015@gmail.com>
2026-06-13 13:14:57 +0100

    date counter: 3

--- fundamtal/date_counter/src/App.tsx
+++ fundamtal/date_counter/src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [step, setStep] = useState<number>(1);
+
 	return (
 		<>
-			<p>Hello</p>
+			<input type="range" min="0" max="10" value="1" step="1" />
+			{/* step */}
 		</>
 	);
 }

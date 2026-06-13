f10f6acefe5c573ad293ec68fc1ed057e1ec9789
araborg <arababs2015@gmail.com>
2026-06-13 22:42:57 +0100

    date counter: 21

--- fundamtal/date_counter/src/date.tsx
+++ fundamtal/date_counter/src/date.tsx
@@ -1,5 +1,6 @@
 export function date(days: number): string {
-	const today = new Date("2027-06-21");
+	const today = new Date();
+	// const today = new Date("2027-06-21");
 
 	const futureDate = new Date(today);
 	futureDate.setDate(futureDate.getDate() + days);

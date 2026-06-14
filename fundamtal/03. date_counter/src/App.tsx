fea399bfdb4f217c300978be6ca9c7a24c79d1cd
araborg <arababs2015@gmail.com>
2026-06-14 07:41:30 +0100

    date counter: 28

--- fundamtal/03. date_counter/src/App.tsx
+++ fundamtal/03. date_counter/src/App.tsx
@@ -1,59 +1,7 @@
-import { useState } from "react";
-
-import { date } from "./date";
-
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<div>
-				<input
-					type="range"
-					min="0"
-					max="10"
-					value={step}
-					onChange={(e) => setStep(Number(e.target.value))}
-				/>
-
-				<span>Step: {step}</span>
-			</div>
-
-			<div>
-				<button value={nums} onClick={() => setNums(nums - step)}>
-					-
-				</button>
-
-				<input
-					type="text" // ds is impt
-					value={nums}
-					onChange={(e) => setNums(Number(e.target.value))}
-				/>
-
-				<button value={nums} onClick={() => setNums(nums + step)}>
-					+
-				</button>
-			</div>
-
-			<div>
-				<p>{date(nums)}</p>
-			</div>
-
-			{nums > 0 ? (
-				<button
-					onClick={() => {
-						setNums(0);
-
-						setStep(1);
-					}}
-				>
-					Reset
-				</button>
-			) : (
-				""
-			)}
-		</>
-	);
+	return <p>hello</p>;
 }
 
 export default App;

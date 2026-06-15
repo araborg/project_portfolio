4d3a97f79c200dc6f594b7ac04b0f94fbb8e6a43
araborg <arababs2015@gmail.com>
2026-06-15 21:29:07 +0100

    accordion: 3

--- fundamtal/04. accordion/src/App.tsx
+++ fundamtal/04. accordion/src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
+import { faqs } from "./faqs/faqs";
+
 function App() {
-	return <p>Hello</p>;
+	return (
+		<>
+			{/*  */}
+			{/* faqs.map(faq=>(console.log(faq))) */}
+			{console.log(faqs)}
+			{/*  */}
+
+			{faqs.map((faq) => console.log(faq))}
+		</>
+	);
 }
 
 export default App;

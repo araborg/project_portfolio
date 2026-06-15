e20a80deb1e1b1971f79c393598b5966266c0e94
araborg <arababs2015@gmail.com>
2026-06-15 12:21:48 +0100

    date counter: 33

--- fundamtal/03. date_counter/src/App.tsx
+++ fundamtal/03. date_counter/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Input from "./Input";
+import Buttons from "./Buttons";
 
 function App() {
 	const [range, setRange] = useState<number>(1);
 	const [nums, setNums] = useState<number>(0);
 	// console.log(range);
+
+	const decrement = () => setNums((prev: number) => prev - 1);
+	const increment = () => setNums((prev: number) => prev + 1);
 
 	return (
 		<>
@@ -16,7 +20,11 @@ function App() {
 			</div>
 
 			<div>
+				<Buttons text="-" fxn={decrement} />
+
 				<Input type="text" value={nums} fxn={setNums} />
+
+				<Buttons text="+" fxn={increment} />
 			</div>
 		</>
 	);
--- fundamtal/03. date_counter/src/Buttons.tsx
+++ fundamtal/03. date_counter/src/Buttons.tsx
@@ -1,3 +1,10 @@
-function Buttons() {}
+interface ButtonsProps {
+	text: string;
+	fxn: () => void;
+}
+
+function Buttons({ text, fxn }: ButtonsProps) {
+	return <button onClick={fxn}>{text}</button>;
+}
 
 export default Buttons;

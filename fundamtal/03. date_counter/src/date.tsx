13bf67f79cf78e24ff8f4f89d8efbf774e3950df
araborg <arababs2015@gmail.com>
2026-06-15 16:14:36 +0100

    date counter: 40

--- fundamtal/03. date_counter/src/date.tsx
+++ fundamtal/03. date_counter/src/date.tsx
@@ -1,7 +1,8 @@
 // 2027-06-21
 function date(nums: number): string {
 	const currDate = new Date("2027-06-21");
-	const fetchDate = currDate.setDate(currDate.getDate() + nums);
+	// const fetchDate = currDate.setDate(currDate.getDate() + nums);
+	const fetchDate = currDate.getDate() + nums;
 
 	const newDate = fetchDate === 0 ? "Today is " : "is Mon Jun 21 2027";
 

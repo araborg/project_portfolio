043311b9a0fb087ca8ab15f718431a5539ba620f
araborg <arababs2015@gmail.com>
2026-06-16 21:41:59 +0100

    accordion: 9

--- fundamtal/04. accordion/src/FaQuestionsItem.tsx
+++ fundamtal/04. accordion/src/FaQuestionsItem.tsx
@@ -1,15 +1,19 @@
-type FaQuestionsItemsProps = {
+// type FaQuestionsItemsProps = {
+// 	title: string;
+// 	text: string;
+// };
+
+interface FaQuestionsItemsProps {
 	title: string;
 	text: string;
-};
+}
 
 function FaQuestionsItem({ title, text }: FaQuestionsItemsProps) {
 	return (
-		//
 		<div>
 			<p>{title}</p>
+
 			<p>{text}</p>
-			{/* <p>{id}</p> */}
 		</div>
 	);
 }

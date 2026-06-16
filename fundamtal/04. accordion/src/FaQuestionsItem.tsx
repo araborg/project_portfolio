2d8cee3c3d258ddf2cfdf1c63c461213cff5806c
araborg <arababs2015@gmail.com>
2026-06-16 22:52:44 +0100

    accordion: 10

--- fundamtal/04. accordion/src/FaQuestionsItem.tsx
+++ fundamtal/04. accordion/src/FaQuestionsItem.tsx
@@ -3,12 +3,16 @@
 // 	text: string;
 // };
 
+import { useState } from "react";
+
 interface FaQuestionsItemsProps {
 	title: string;
 	text: string;
 }
 
 function FaQuestionsItem({ title, text }: FaQuestionsItemsProps) {
+	const [open, setOpen] = useState<boolean>(false);
+
 	return (
 		<div>
 			<p>{title}</p>
